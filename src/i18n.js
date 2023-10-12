1d29b86978751094fbfa79ab61b76ee94054a3f6
pavelkhon <va7mont@gmail.com>
2023-10-12 13:52:08 +0500

    Github pages fix

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from 'react-i18next';
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://multi-lang-app-react.netlify.app'
+const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://va7mont.github.io/vuse/'
 
 i18n
   .use(i18nBackend)
