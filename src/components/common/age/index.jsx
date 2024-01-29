d628479fbcbcc0b82e5ede2b85a4c63406499c87
Max Kosheutov <maxkosh1994@cmail.com>
2024-01-29 23:09:52 +0300

    corrected the age limit plate

--- src/components/common/age/index.jsx
+++ src/components/common/age/index.jsx
@@ -1,20 +1,28 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-import age from 'src/assets/images/icons/age.svg'
+import age from 'src/assets/images/icons/age.svg';
+import ageAm from 'src/assets/images/icons/ageAm.svg';
 
 const Age = () => {
+  const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language;
 
-  const {t} = useTranslation()
+  let ageToDisplay;
+  if (currentLanguage === 'am') {
+    ageToDisplay = ageAm;
+  } else {
+    ageToDisplay = age;
+  }
 
   return (
     <div className={'sticky bottom-0 z-30'}>
       <div className={'flex items-center justify-center bg-primary py-1 px-3'}>
-        <img src={age} className={'me-3'} alt="age"/>
+        <img src={ageToDisplay} className={'me-3'} alt='age' />
         <p className={'text-white text-xs lg:text-base uppercase'}>
           {t(`restriction.age`)}
         </p>
       </div>
     </div>
-  )
-}
-export default Age
+  );
+};
+export default Age;
