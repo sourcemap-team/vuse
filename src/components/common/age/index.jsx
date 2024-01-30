2c786074fa69bfcf4f8bceca805ac65aba7dd8bd
Max Kosheutov <maxkosh1994@cmail.com>
2024-01-30 10:29:11 +0300

    fix image in restriction banner

--- src/components/common/age/index.jsx
+++ src/components/common/age/index.jsx
@@ -1,23 +1,23 @@
 import { useTranslation } from 'react-i18next';
 
-import age from 'src/assets/images/icons/age.svg';
+// import age from 'src/assets/images/icons/age.svg';
 import ageAm from 'src/assets/images/icons/ageAm.svg';
 
 const Age = () => {
-  const { t, i18n } = useTranslation();
-  const currentLanguage = i18n.language;
+  const { t } = useTranslation();
+  //   const currentLanguage = i18n.language;
 
-  let ageToDisplay;
-  if (currentLanguage === 'am') {
-    ageToDisplay = ageAm;
-  } else {
-    ageToDisplay = age;
-  }
+  //   let ageToDisplay;
+  //   if (currentLanguage === 'am') {
+  //     ageToDisplay = ageAm;
+  //   } else {
+  //     ageToDisplay = age;
+  //   }
 
   return (
     <div className={'sticky bottom-0 z-30'}>
       <div className={'flex items-center justify-center bg-primary py-1 px-3'}>
-        <img src={ageToDisplay} className={'me-3'} alt='age' />
+        <img src={ageAm} className={'me-3'} alt='age' />
         <p className={'text-white text-xs lg:text-base uppercase'}>
           {t(`restriction.age`)}
         </p>
